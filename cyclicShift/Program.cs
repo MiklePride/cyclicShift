2e9de15eacfbd15816feaec417c4b927d5506ade
Михаил Денисов <migelius.invesy@gmail.com>
2022-06-08 13:19:20 +0300

    Добавьте файлы проекта.

---
+++ cyclicShift/Program.cs
@@ -0,0 +1,37 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayNumber = { 1, 2, 3, 4, 5};
+        int countShift = 1;
+        int temporary = 0;
+
+        for (int j = countShift; j > 0; j--)
+        {
+            for (int i = 0; i < arrayNumber.Length - 1; i++)
+            {
+                if (arrayNumber[i] == arrayNumber[0])
+                {
+                    temporary = arrayNumber[i];
+                    arrayNumber[i] = arrayNumber[i + 1];
+                }
+                else
+                {
+                    arrayNumber[i] = arrayNumber[i + 1];
+                }
+               
+
+                if (arrayNumber[i] == arrayNumber.Length - 1)
+                {
+                    arrayNumber[i] = temporary;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < arrayNumber.Length; i++)
+        {
+            Console.WriteLine(arrayNumber[i]);
+        }
+    }
+}
